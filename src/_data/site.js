7eb06ef8a2b9c13ed2698678753f1f17665891b9
Trebel <trebel@defiplaza.net>
2022-01-31 17:00:10 +0100

    fix(*): more visual fixes

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
 	title: 'DefiPlaza',
-	description: 'the low-cost exchange',
+	description: 'The low-cost exchange on Ethereum',
 	url: process.env.SITE_URL,
 	icon: process.env.SITE_URL + '/assets/images/logo/icon-dark@3x.png',
 	cover_image: process.env.SITE_URL + '/assets/images/logo/logo@3x.png',
