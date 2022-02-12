371c9bca7cefd5012e9b157a0ae80b7984333894
Trebel <trebel@defiplaza.net>
2022-02-12 18:13:20 +0100

    fix(*): use svg as site icon

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,7 +4,7 @@ module.exports = {
 	title: 'DefiPlaza',
 	description: 'The low-cost exchange on Ethereum',
 	url: process.env.SITE_URL,
-	icon: process.env.SITE_URL + '/assets/images/logo/icon-dark@3x.png',
+	icon: process.env.SITE_URL + '/assets/images/logo/icon.svg',
 	cover_image: process.env.SITE_URL + '/assets/images/logo/social-media.png',
 	lang: 'en_US',
 	twitter: '@defiplaza',
