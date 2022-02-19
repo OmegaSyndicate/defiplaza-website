eba35605a0101b11d6aa3de8a5a1ca4c73d3315b
Trebel <trebel@defiplaza.net>
2022-02-19 12:53:09 +0100

    fix(social-media): update social media share image

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
 	title: 'DefiPlaza',
-	description: 'The low-cost exchange on Ethereum',
+	description: 'The lowest fees of any exchange on Ethereum',
 	url: process.env.SITE_URL,
 	icon: process.env.SITE_URL + '/assets/images/logo/icon.svg',
-	cover_image: process.env.SITE_URL + '/assets/images/logo/social-media.png',
+	cover_image: process.env.SITE_URL + '/assets/images/logo/social-media-share.png',
 	lang: 'en_US',
 	twitter: '@defiplaza',
 };
